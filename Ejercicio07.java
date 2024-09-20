5ada476cc09a4c67ba6d51e1ad3f416cb3c5decd
Ruben_BV <r.blanco.3d@gmail.com>
2024-09-20 13:45:38 +0200

    */ Ejercicio 07

--- Ejercicio07.java
+++ Ejercicio07.java
@@ -6,7 +6,20 @@ public class Ejercicio07 {
 
         Scanner scanner = new Scanner(System.in);
 
-        
+        System.out.println("Vamos a calcular la siguiente expresión: x + y * z");
+        System.out.println("Introduce por favor, el valor deseado para x: ");
+        double x = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.println("Introduce por favor, el valor deseado para y: ");
+        double y = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.println("Introduce por favor, el valor deseado para z: ");
+        double z = scanner.nextDouble();
+        scanner.nextLine();
+
+        double resultado = x + y * z;
+
+        System.out.println("El resultado de la operación es: " + resultado);
 
         scanner.close();
     }
