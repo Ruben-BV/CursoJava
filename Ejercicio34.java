7a07f72611fd187c33a5d4a9a96d53468a27a86f
Ruben_BV <r.blanco.3d@gmail.com>
2024-09-29 01:28:52 +0200

    */ Ejercicio34

---
+++ Ejercicio34.java
@@ -0,0 +1,11 @@
+public class Ejercicio34 {
+    public static void main(String[] args) {
+        
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+        System.out.println((int)Character.MAX_VALUE);
+        System.out.println((int) Character.MIN_VALUE);
+    }
+}
