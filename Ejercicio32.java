ee9814975fe50bdeee7dc62f7e02a708a59e9025
Ruben_BV <r.blanco.3d@gmail.com>
2024-09-28 11:03:48 +0200

    */ Ejercicio31

---
+++ Ejercicio32.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+public class Ejercicio32 {
+    
+    public static void main(String[] args) {
+
+        int counter = 0;
+        String vocales = "AEIOUaeiou";
+
+        String cadena = JOptionPane.showInputDialog("Introduce una cadena de texto y te diré cuántas vocales tiene:");
+
+        for(int i=0; i < cadena.length(); i++) {
+            char caracter = cadena.charAt(i);
+            for(int j=0; j < vocales.length(); j++){
+                char vocal = vocales.charAt(j);
+                if(caracter == vocal){
+                    counter = counter + 1;
+                }
+            }
+        }
+        
+        if(counter==0){JOptionPane.showMessageDialog(null,"Tu cadena no tiene vocales.");}
+        else if(counter==1){JOptionPane.showMessageDialog(null,"Tu cadena tiene: " + counter +" vocal.");}
+        else {JOptionPane.showMessageDialog(null,"Tu cadena tiene: " + counter +" vocales.");}
+        
+        
+    }
+}
